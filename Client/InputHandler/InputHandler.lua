dfa889c965816cd304542e2f52dcdc451399ba4e
NegativeName <negativenamengt@gmail.com>
2024-04-26 20:21:53 +0200

    Update 1.1.6

--- Client/InputHandler/InputHandler.lua
+++ Client/InputHandler/InputHandler.lua
@@ -3,24 +3,36 @@ local tWidgetToHandle = {}
 
 -- Re-calculates the input rules
 function WGUI.HandleInputRules()
-    local bMouseEnabled = WGUI.GetInputRules()
+    local bMouseEnabled, bKeyboardEnabled = WGUI.GetInputRules()
 
     Input.SetMouseEnabled(bMouseEnabled)
+    Input.SetInputEnabled(bKeyboardEnabled)
 end
 
 -- Returns the input rules.
 ---@return boolean @ Whether the mouse should be enabled
+---@return boolean @ Whether the keyboard should be enabled
 function WGUI.GetInputRules()
     local bMouseEnabled = false
+    local bKeyboardEnabled = true
 
-    for oUI, _ in pairs(tWidgetToHandle) do
-        if oUI:GetMouseEnabled() and oUI:IsVisible() then
-            bMouseEnabled = true
-            break
+    for oWidget, _ in pairs(tWidgetToHandle) do
+        if not oWidget:IsVisible() then
+            goto continue
         end
+
+        if not bMouseEnabled and oWidget:GetMouseEnabled() then
+            bMouseEnabled = true
+        end
+
+        if bKeyboardEnabled and not oWidget:GetKeyboardEnabled() then
+            bKeyboardEnabled = false
+        end
+
+        ::continue::
     end
 
-    return bMouseEnabled
+    return bMouseEnabled, bKeyboardEnabled
 end
 
 -- Whether or not the mouse should be enabled for this widget.
@@ -28,10 +40,10 @@ end
 function BaseWidget:SetMouseEnabled(bIsEnabled)
     self:SetValue("__IsMouseEnabled", bIsEnabled)
 
-    if bIsEnabled then
-        tWidgetToHandle[self] = true
-    else
+    if not bIsEnabled and self:GetKeyboardEnabled() then
         tWidgetToHandle[self] = nil
+    else
+        tWidgetToHandle[self] = true
     end
 
     WGUI.HandleInputRules()
@@ -43,5 +55,30 @@ function BaseWidget:GetMouseEnabled()
     return self:GetValue("__IsMouseEnabled", false)
 end
 
+-- Whether or not the keyboard should be enabled for this widget.
+---@param bIsEnabled boolean
+function BaseWidget:SetKeyboardEnabled(bIsEnabled)
+    self:SetValue("__IsKeyboardEnabled", bIsEnabled)
+
+    if not bIsEnabled and not self:GetMouseEnabled() then
+        tWidgetToHandle[self] = nil
+    else
+        tWidgetToHandle[self] = true
+    end
+
+    WGUI.HandleInputRules()
+end
+
+-- Returns whether or not the keyboard is enabled for this widget.
+---@return boolean
+function BaseWidget:GetKeyboardEnabled()
+    return self:GetValue("__IsKeyboardEnabled", true)
+end
+
 Events.Subscribe("WGUI::WidgetVisibilityChanged", WGUI.HandleInputRules)
-BaseWidget.Subscribe("Destroy", function (self) self:SetMouseEnabled(false) end)
+BaseWidget.Subscribe("Destroy", function (self)
+    if tWidgetToHandle[self] then
+        tWidgetToHandle[self] = nil
+        WGUI.HandleInputRules()
+    end
+end)
